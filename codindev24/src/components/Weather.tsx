d37760450875b27e06238bf615dde0e92dad8219
Codindev24 <odinnmoddar@gmail.com>
2024-05-05 15:34:31 +0000

    Vedrid ad verda flott

--- codindev24/src/components/Weather.tsx
+++ codindev24/src/components/Weather.tsx
@@ -58,56 +58,39 @@ function Navbar() {
 
            <div className="new">
 
+            <div className="adjust flex justify-evenly">
+
+           <ul className="iconname">
+               <span className="flex justify-center">
+               <li className="icon"><span><img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} /> </span></li>
+               <li className="name"><span>{data.name}</span></li>
+               </span>
+             </ul>
+
+
            <ul className="nameheatclouds">
-               <span className="flex">
-               <li className="name"> <img src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`} height="70px" /> {data.name}</li>
-               <li className="temp">{data.main ? <span><span className="name">Temp</span><span className="temp">{data.main.temp.toFixed()}</span> <span className="cells">°c</span></span> : null}</li>
-               <li className="feels">{data.main ? <span><span className="name">Feels</span> <span className="feels">{data.main.feels_like.toFixed()}</span><span className="cells"> °c</span></span> : null}</li>
+               <span className="flex justify-evenly">
+               <li className="temp">{data.main ? <span><span className="name">Temp</span><span className="temp">{data.main.temp.toFixed()}</span> <p className="cells">°c</p></span> : null}</li>
+               <li className="feels">{data.main ? <span><span className="name">Feels</span> <span className="feels">{data.main.feels_like.toFixed()}</span><p className="cells"> °c</p></span> : null}</li>
                <li className="clouds">{data.weather ? <span>{data.weather[0].main}</span> : null}</li>
-               <li className="wind">{data.wind ? <span><span className="name">Wind</span> <span className="wind">{data.wind.speed.toFixed()}</span><span className="cells">m/s</span></span> : null}</li>
-               <li className="hum">{data.main ? <span><span className="name">Hum</span> <span className="hum">{data.main.humidity}</span><span className="cells">%</span></span> : null}</li>
+               <li className="wind">{data.wind ? <span><span className="name">Wind</span> <span className="wind">{data.wind.speed.toFixed()}</span><p className="cells">m/s</p></span> : null}</li>
+               <li className="hum">{data.main ? <span><span className="name">Hum</span> <span className="hum">{data.main.humidity}</span><p className="cells">%</p></span> : null}</li>
                </span>
              </ul>
              
              <ul className="below">
              <span className="flex justify-between">
-             <li className="mintemp">{data.main ? <span><span className="mintemp">{data.main.temp_min}</span><span className="cells"> °c</span> <br /> <span className="name">Min Temp</span></span> : null}</li>
-             <li className="cmax">{data.main ? <span>Max Temp <span className="tempdatamax">{data.main.temp_max}</span><span className="cels"> °c</span></span> : null}</li>
-             <li className="windgust">{data.wind ? <p className='bold'><span className="wind">Wind gust</span> <span className="winddata">{data.wind.gust.toFixed()}</span> <span className="cells">m/s</span></p> : null}</li>
-             <li className="sea">{data.main ? <p className='bold'><span className="hum">Visibility</span> <span className="tempdatamax">{data.visibility} <span className="km">km</span></span></p> : null}</li>
-             <li className="sea">{data.main ? <span>Sea Level <span className="tempdatamax">{data.main.sea_level}</span></span> : null}</li>
-             <li className="sea">{data.main ? <span>Updated <span className="tempdatamax">{data.lastupdate}</span></span> : null}</li>
+             <li className="mintemp">{data.main ? <span><span className="name">Min</span><span className="mintemp">{data.main.temp_min}</span><p className="cells"> °c</p></span> : null}</li>
+             <li className="maxtemp">{data.main ? <span><span className="name">Max</span> <span className="maxtemp">{data.main.temp_max}</span><p className="cells"> °c</p></span> : null}</li>
+             <li className="windgust">{data.wind ? <span><span className="name">Gust</span> <span className="windgust">{data.wind.gust.toFixed()}</span> <p className="cells">m/s</p></span> : null}</li>
+             <li className="dist">{data.main ? <span><span className="name">Dist</span> <span className="dist">{data.visibility}</span> <p className="cells">km</p></span> : null}</li>
+             <li className="sea">{data.main ? <span><span className="name">Sea </span><span className="sea">{data.main.sea_level}</span><p className="cells">Level</p></span> : null}</li>
              </span>
              </ul>
 
+             </div>{/* .adjust */}
+
            </div>{/* .new */}
-
-           {/* /////////////////////////////////////////////// OLD ///////////////////////////////////////////////////////////////////////// */}
-
-            <div className="left">
-              <ul className="nameheatclouds">
-                <span className="flex justify-between">
-                <li className="name">{data.name}</li>
-                <li>{data.wind.direction}</li>
-                <li className="c">{data.main ? <span>Temp <span className="tempdata">{data.main.temp.toFixed()}</span><span className="cels"> °c</span></span> : null}</li>
-                <li className="feels">{data.main ? <p className=''>Feels <span className="feelsdata">{data.main.feels_like.toFixed()}</span><span className="cels"> °c</span></p> : null}</li>
-                <li className="snow">{data.weather ? <p>{data.weather[0].main}</p> : null}</li>
-                <li className="w">{data.wind ? <p className='bold'><span className="wind">Wind</span> <span className="winddata">{data.wind.speed.toFixed()}</span> <span className="cells"><span className="ms">m/s</span></span></p> : null}</li>
-                <li className="hum">{data.main ? <p className='bold'><span className="hum">Hum</span> <span className="numb">{data.main.humidity}</span><span className="perc">%</span></p> : null}</li>
-                </span>
-              </ul>
-              <ul className="below">
-              <span className="flex justify-between">
-              <li className="cmin">{data.main ? <span>Min Temp <span className="tempdatamin">{data.main.temp_min}</span><span className="cels"> °c</span></span> : null}</li>
-              <li className="cmax">{data.main ? <span>Max Temp <span className="tempdatamax">{data.main.temp_max}</span><span className="cels"> °c</span></span> : null}</li>
-              <li className="windgust">{data.wind ? <p className='bold'><span className="wind">Wind gust</span> <span className="winddata">{data.wind.gust.toFixed()}</span> <span className="cells">m/s</span></p> : null}</li>
-              <li className="sea">{data.main ? <p className='bold'><span className="hum">Visibility</span> <span className="tempdatamax">{data.visibility} <span className="km">km</span></span></p> : null}</li>
-              <li className="sea">{data.main ? <span>Sea Level <span className="tempdatamax">{data.main.sea_level}</span></span> : null}</li>
-              <li className="sea">{data.main ? <span>Updated <span className="tempdatamax">{data.lastupdate}</span></span> : null}</li>
-              </span>
-              </ul>
-
-            </div>{/* .left */}
 
           </div>/* .vedrid */
         }
