1b33730d652a0496c793e9a8fc783adc09e26be9
Codindev24 <odinnmoddar@gmail.com>
2024-05-16 19:21:38 +0000

    fixed signout displaying when not logged in

--- codindev24/src/components/Navbar.tsx
+++ codindev24/src/components/Navbar.tsx
@@ -101,9 +101,13 @@ export default function Navbar() {
             tabIndex={0}
             className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52 absolute z-20"
           >
-            <li>
+             {!user ? (
+              <div></div>
+              ) : (
+              <li>
               <a href="/login" onClick={logout}><LogoutIcon />Signout</a>
-            </li>
+              </li>
+              )}
             <li>
               <a><EmailIcon/>Contact</a>
             </li>
